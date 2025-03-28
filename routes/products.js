0f3f78e0fe4582aa5de64814a93bd7b73539fbac
Andy Hall <andersonhall@gmail.com>
2025-03-28 16:58:40 -0500

    implement create product

--- routes/products.js
+++ routes/products.js
@@ -23,4 +23,34 @@ router.get("/:id", async (req, res) => {
   res.json(product.rows);
 });
 
+router.post("/", async (req, res) => {
+  const { product_name, product_desc, sku, price, categories } = req.body;
+  const product = await db.query("SELECT * FROM product WHERE sku = $1", [sku]);
+  if (product.rows.length > 0) {
+    return res.json({ msg: "Product already exists." });
+  }
+  const inventoryId = await db.query(
+    "INSERT INTO product_inventory DEFAULT VALUES RETURNING id"
+  );
+  const newProductId = await db.query(
+    "INSERT INTO product (product_name, product_desc, inventory_id, sku, price) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+    [product_name, product_desc, inventoryId.rows[0].id, sku, price]
+  );
+  if (categories.length > 0) {
+    // check for existing
+    for (const category of categories) {
+      let cat = await db.query("SELECT * FROM product_category WHERE id = $1", [
+        category,
+      ]);
+      if (cat.rows.length > 0) {
+        await db.query(
+          "INSERT INTO products_categories (product_id, category_id) VALUES ($1, $2)",
+          [newProductId.rows[0].id, category]
+        );
+      }
+    }
+  }
+  res.json({ message: "Product created." });
+});
+
 module.exports = router;
