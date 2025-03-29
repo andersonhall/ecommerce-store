3eee9ce10e8662d0f797dec34ab8c530c96a2e93
Andy Hall <andersonhall@gmail.com>
2025-03-29 10:34:01 -0500

    implement update products and delete products

--- routes/products.js
+++ routes/products.js
@@ -52,4 +52,69 @@ router.post("/", async (req, res) => {
   res.json({ message: "Product created." });
 });
 
+router.put("/:id", async (req, res) => {
+  const { product_name, product_desc, sku, price, categories } = req.body;
+  if (product_name !== undefined) {
+    const newName = product_name;
+    await db.query(
+      "UPDATE product SET product_name = $1, modified_at = now() WHERE id = $2 AND product_name <> $1",
+      [newName, req.params.id]
+    );
+  }
+  if (product_desc !== undefined) {
+    const newDesc = product_desc;
+    await db.query(
+      "UPDATE product SET product_desc = $1, modified_at = now() WHERE id = $2 AND product_desc <> $1",
+      [newDesc, req.params.id]
+    );
+  }
+  if (sku !== undefined) {
+    const newSku = sku;
+    await db.query(
+      "UPDATE product SET sku = $1, modified_at = now() WHERE id = $2 AND sku <> $1",
+      [newSku, req.params.id]
+    );
+  }
+  if (price !== undefined) {
+    const newPrice = price;
+    await db.query(
+      "UPDATE product SET price = $1, modified_at = now() WHERE id = $2 AND price <> $1",
+      [newPrice, req.params.id]
+    );
+  }
+  if (categories !== undefined) {
+    const newCategories = categories;
+    await db.query("DELETE FROM products_categories WHERE product_id = $1", [
+      req.params.id,
+    ]);
+    for (const category of newCategories) {
+      let cat = await db.query("SELECT * FROM product_category WHERE id = $1", [
+        category,
+      ]);
+      if (cat.rows.length > 0) {
+        await db.query(
+          "INSERT INTO products_categories (product_id, category_id) VALUES ($1, $2)",
+          [req.params.id, category]
+        );
+      }
+    }
+  }
+  const product = await db.query("SELECT * FROM product WHERE id = $1", [
+    req.params.id,
+  ]);
+  res.json({ message: "Product updated.", product: product.rows[0] });
+});
+
+router.delete("/:id", async (req, res) => {
+  const productToDelete = await db.query(
+    "SELECT * FROM product WHERE id = $1",
+    [req.params.id]
+  );
+  if (productToDelete.rows.length === 0) {
+    return res.json({ message: "Product with that ID doesn't exist." });
+  }
+
+  res.json({ message: "Product deleted." });
+});
+
 module.exports = router;
